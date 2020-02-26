f465f5d5ef734ef79a962990a49715b88d51faa6
Anastasiya <mahanasty@mail.ru>
2020-02-26 23:56:32 +0300

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,35 @@
-/*$(document).ready(function() {
-    $('header_burger').click(function(event) {
-        $('header_burger', 'header_menu').toggleClass('active');
-    });
+let btnBurger = document.querySelector('.header-burger');
 
-});*/
+btnBurger.addEventListener('click', function() {
+    let mobMenu = document.querySelector('.header-menu');
+
+    btnBurger.classList.toggle('burger-menu-active');
+    mobMenu.classList.toggle('show-menu');
+});
+
 
 $(document).ready(function() {
     $('.slider').slick({
-        dots: true,
-        autoplay: false,
-        autoplaySpeed: 500
+        dots: false,
+        autoplay: false
     });
+});
+
+$(document).ready(function() {
+    $('.slider-wr').slick({
+        dots: false,
+        autoplay: false
+    });
+});
+
+$(document).ready(function() {
+    $(".fancybox").fancybox();
+});
+
+$('input,textarea').focus(function() {
+    $(this).data('placeholder', $(this).attr('placeholder'))
+    $(this).attr('placeholder', '');
+});
+$('input,textarea').blur(function() {
+    $(this).attr('placeholder', $(this).data('placeholder'));
 });
