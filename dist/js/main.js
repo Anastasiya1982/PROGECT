ed9b4d516f851e9f82978d975ac13d16b325d2ba
Anastasiya1982 <mahanasty@mail.ru>
2020-03-12 22:08:04 +0300

    add form in team file

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,10 @@
+$(window).on('load', function() {
+    var $preloader = $('#p_prldr'),
+        $svg_anm = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(1000).fadeOut('slow');
+});
+
 let btnBurger = document.querySelector('.header-burger');
 
 btnBurger.addEventListener('click', function() {
@@ -5,6 +12,26 @@ btnBurger.addEventListener('click', function() {
 
     btnBurger.classList.toggle('burger-menu-active');
     mobMenu.classList.toggle('show-menu');
+});
+$(document).ready(function() {
+    $('[data-fancybox="gallery"]').fancybox({
+        loop: true,
+        gutter: 10,
+        keyboard: true,
+        arrows: true,
+        infobar: true,
+        smallBtn: true,
+        toolbar: true,
+        idleTime: 3,
+        animationEffect: "zoom-in-out",
+        animationDuration: 600,
+        transitionEffect: "circular",
+        transitionDuration: 600,
+        slideClass: "myClass",
+        baseClass: "myclass",
+
+
+    });
 });
 
 
@@ -42,43 +69,7 @@ $('.team-slider').slick({
 });
 
 
-$(document).ready(function() {
-    $('[data-fancybox="gallery"]').fancybox({
-        buttons: [
-            'slideShow',
-            'share',
-            'zoom',
-            'fullScreen',
-            'download',
-            'close'
-        ],
-        loop: true,
-        gutter: 10,
-        keyboard: true,
-        arrows: true,
-        infobar: true,
-        smallBtn: false,
-        toolbar: true,
-        idleTime: 3,
-        animationEffect: "zoom-in-out",
-        animationDuration: 600,
-        transitionEffect: "tube",
-        transitionDuration: 600,
-        slideClass: "myClass",
-        baseClass: "myclass",
-        slideShow: {
-            autoStart: false,
-            speed: 1000
-        },
-        youtube: {
-            controls: 0,
-            showinfo: 0
-        },
-        thumbs: {
-            autoStart: true
-        }
-    });
-});
+
 
 $('input,textarea').focus(function() {
     $(this).data('placeholder', $(this).attr('placeholder'))
